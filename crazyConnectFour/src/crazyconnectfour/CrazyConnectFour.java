135187f79b21ac309f627ff01fd53b006ea456c5
wall-terry <wal12073@byui.edu>
2014-01-25 15:44:24 -0800

    I have added instance variables and two methods to the CrazyConnectFour Class.  I modified Main to take the name entry and to output the instructions.

--- crazyConnectFour/src/crazyconnectfour/CrazyConnectFour.java
+++ crazyConnectFour/src/crazyconnectfour/CrazyConnectFour.java
@@ -6,17 +6,37 @@
 
 package crazyconnectfour;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Terry Wall
  */
 public class CrazyConnectFour {
-
-    /**
-     * @param args the command line arguments
-     */
+    // Instance variables
+    String name;
+    String instructions = "This is a modified version of the game connect four \n\n"
+            + "You try to get four tokens in a row either up-and-down, left-to-right, or diagonally. \n"
+            + "The added twist is once you have four in a row the tokens disappear and the tokens above them \n"
+            + "drop to fill in the space.  This will likely result in new combinations of four or more which will also \n"
+            + "disappear and result in a similar cascade which will continue until no more combinations of four or more \n"
+            + "are created.";
+    
     public static void main(String[] args) {
-        System.out.println("This is the crazyConnectFour Main Class");
+        CrazyConnectFour playTheGame = new CrazyConnectFour();
+        playTheGame.getName();
+        playTheGame.displayHelp();
     }
     
+    public void getName(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        this.name = input.next();
+        
+    }
+    
+    public void displayHelp(){
+        System.out.println("\nWelcome " + this.name +"\n ");
+        System.out.println(this.instructions);
+    }
 }
