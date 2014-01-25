1abdb9e6880ace4015518905fbe17c95b279eb54
wall-terry <wal12073@byui.edu>
2014-01-25 11:03:52 -0800

    This is my first commit

---
+++ crazyConnectFour/src/crazyconnectfour/CrazyConnectFour.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package crazyconnectfour;
+
+/**
+ *
+ * @author Terry Wall
+ */
+public class CrazyConnectFour {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is the crazyConnectFour Main Class");
+    }
+    
+}
