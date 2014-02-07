18e1faa14a0329b05fedd065a3a06c2e83474ceb
dccoatney <dccoatney@192.168.1.124>
2014-02-07 11:16:39 -0800

    Merge origin/master
    Conflicts:
    	crazyConnectFour/src/HelpMenuControl.java
    	crazyConnectFour/src/crazyconnectfour/HelpMenuControl.java

---
+++ crazyConnectFour/src/crazyconnectfour/GamePreferencesMenuView.java
@@ -0,0 +1,97 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 dccoatney.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ *
+ * @author dccoatney
+ */
+
+package crazyconnectfour;
+
+import java.util.Scanner;
+
+
+public class GamePreferencesMenuView {
+     Game game;
+    private GamePreferencesMenuControl gamePreferenceControl = new GamePreferencesMenuControl(game);
+
+    private final static String[][] menuItems = {
+        {"1", "Change Marker of the first Player"},
+        {"2", "Change Marker of the second Player"},
+        {"D", "Change the dimensions of the board"},
+        {"Q", "Return to game menu"}
+    };
+
+    public GamePreferencesMenuView(Game game) {
+        this.game = game;
+    }
+
+    
+    public void getInput() {       
+        String command;
+        Scanner inFile = new Scanner(System.in);
+        
+        do {
+            this.display();
+
+            // get commaned entered
+            command = inFile.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "1":
+                    this.gamePreferenceControl.getMarker(this.game.playerA);
+                    break;
+                case "2":
+                    this.gamePreferenceControl.getMarker(this.game.playerB);
+                    break;
+                case "D":
+                    this.gamePreferenceControl.getDimensions();
+                    break;
+                case "Q":
+                    break;
+                default: 
+                    new crazyconnectfour().displayError("Invalid command. Please enter a valid command.");
+                    continue;
+            }
+        } while (!command.equals("Q"));
+
+        return;
+    }
+    
+    
+        
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < GamePreferencesMenuView.menuItems.length; i++) {
+            System.out.println("\t   " + menuItems[i][0] + "\t" + menuItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+ 
+}
+
+
