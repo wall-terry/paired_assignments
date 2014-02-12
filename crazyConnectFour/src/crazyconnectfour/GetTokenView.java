00538bf147a159630a3ebebc474318926c5ccada
dccoatney <dccoatney@192.168.1.124>
2014-02-12 09:50:55 -0800

    Merge origin/master
    Conflicts:
    	crazyConnectFour/src/HelpMenuControl.java
    	crazyConnectFour/src/crazyconnectfour/HelpMenuControl.java

--- crazyConnectFour/src/crazyconnectfour/GetTokenView.java
+++ crazyConnectFour/src/crazyconnectfour/GetTokenView.java
@@ -26,7 +26,57 @@ package crazyconnectfour;
 
 /**
  *
- * @author dccoatney, Terry Wall
+ * @author dccoatney, Terry Wallimport java.util.Scanner;
+
+*/
+
+public class GetTokenView {
+    public GetTokenView ) {
+        this.game = game;
+}
+public String getInput(Player player) {
+
+        String newMarker = null;
+        Scanner in = new Scanner(System.in);
+                
+        boolean valid = false; // flag to indicate if valid character entered
+        while (!valid) {
+            // prompt for input
+            System.out.println("\n\t" + player.name 
+                    + ", enter a single character that will be used to mark "
+                    + "your squares in the game.");
+            
+            // get input from user           
+            newMarker = in.nextLine();
+           
+            // no marker entered?
+            if (newMarker == null  || newMarker.length() < 1) {
+                continue;
+            }
+            
+            // grab only the first character and convert it to upper case
+            newMarker = newMarker.substring(0, 1).toUpperCase();
+            
+            if (newMarker.equals("Q")) { // Quit?
+                return null;
+            }
+            
+            // Check to see if the marker is already in use
+            if (this.game.playerA.marker.equals(newMarker) ||
+                this.game.playerB.marker.equals(newMarker) ) {
+               new CrazyConnectFourError().displayError(
+                        "This marker is currently in use. " +
+                        "Select a different character");
+               continue; 
+            }
+            
+            valid = true; // signal that a valid marker was entered
+        }
+        
+        return newMarker;
+    }
+    
+}
  */
 public class GetTokenView {
     
