4f7f97f3c5636cadbc74f96ac74979d4825fbb1c
Terry Wall <twall411@roadrunner.com>
2014-02-13 13:50:27 -0800

    getTokenView now changes players tokens options.

--- crazyConnectFour/src/crazyconnectfour/GetTokenView.java
+++ crazyConnectFour/src/crazyconnectfour/GetTokenView.java
@@ -36,7 +36,7 @@ public class GetTokenView {
     Game game = new Game();
     Player player = new Player();
     
-    private final static String[][] getToken = {
+    private final static String[][] tokenOptions = {
         {"R", "RED"},
         {"B", "BLACK"},
         {"G", "GREEN"},
@@ -83,6 +83,23 @@ public class GetTokenView {
             if (newToken.equals("Q")) { // Quit?
                 return null;
             }
+           int i = 0;
+           boolean breakout = false;
+           do{
+          
+                if(newToken.equals(tokenOptions[i][0])) {
+                    newToken = tokenOptions [i][1];
+                    breakout = true;
+                   }
+                i++;
+
+                
+            } while (tokenOptions.length != i && !breakout) ;
+           
+            if (i == tokenOptions.length){
+                new CrazyConnectFourError().displayError("\n\t Invalid Entry Try Again");
+                continue;
+            }
             
             // Check to see if the token is already in use
             if (this.game.playerA.token.equals(newToken) ||
@@ -103,8 +120,8 @@ public class GetTokenView {
         System.out.println("\n\t===============================================================");
         System.out.println("\tEnter the letter associated with one of the following Token Options:");
 
-        for (int i = 0; i < GetTokenView.getToken.length; i++) {
-            System.out.println("\t   " + getToken[i][0] + "\t" + getToken[i][1]);
+        for (int i = 0; i < GetTokenView.tokenOptions.length; i++) {
+            System.out.println("\t   " + tokenOptions[i][0] + "\t" + tokenOptions[i][1]);
         }
         System.out.println("\t===============================================================\n");
     }
