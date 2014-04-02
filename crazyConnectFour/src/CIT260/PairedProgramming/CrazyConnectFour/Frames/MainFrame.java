9d1ab4c28b0526577396dd59c1da0e0aefa650f1
dccoatney <dccoatney@192.168.1.124>
2014-04-02 10:48:59 -0700

    Merge origin/master
    Conflicts:
    	crazyConnectFour/src/CIT260/PairedProgramming/CrazyConnectFour/Controls/CrazyConnectFour.java

--- crazyConnectFour/src/CIT260/PairedProgramming/CrazyConnectFour/Frames/MainFrame.java
+++ crazyConnectFour/src/CIT260/PairedProgramming/CrazyConnectFour/Frames/MainFrame.java
@@ -208,7 +208,8 @@ private MainMenuControl mainCommands = new MainMenuControl();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        Help helpFrame = new Help();  
+        helpFrame.setVisible(true);// TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
