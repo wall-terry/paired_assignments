f2f51f706fa3b656b22d36393aae8eab530f2057
dccoatney <dccoatney@192.168.1.124>
2014-04-05 12:46:16 -0700

    Merge origin/master
    Conflicts:
    	crazyConnectFour/src/CIT260/PairedProgramming/CrazyConnectFour/Controls/CrazyConnectFour.java

--- crazyConnectFour/src/CIT260/PairedProgramming/CrazyConnectFour/Frames/GamePreferencesFrame.java
+++ crazyConnectFour/src/CIT260/PairedProgramming/CrazyConnectFour/Frames/GamePreferencesFrame.java
@@ -87,7 +87,7 @@ public class GamePreferencesFrame extends javax.swing.JFrame {
         jTextField8 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        jText1 = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
         jTextField3 = new javax.swing.JTextField();
@@ -327,9 +327,9 @@ public class GamePreferencesFrame extends javax.swing.JFrame {
 
         jLabel4.setText("Player Two:");
 
-        jTextField2.addFocusListener(new java.awt.event.FocusAdapter() {
+        jText1.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
-                jTextField2FocusGained(evt);
+                jText1FocusGained(evt);
             }
         });
 
@@ -366,7 +366,7 @@ public class GamePreferencesFrame extends javax.swing.JFrame {
                     .addGroup(jPanel6Layout.createSequentialGroup()
                         .addComponent(jLabel8)
                         .addGap(39, 39, 39)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jText1, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabelColumns)
@@ -395,7 +395,7 @@ public class GamePreferencesFrame extends javax.swing.JFrame {
                     .addComponent(jLabelPlayerOne))
                 .addGap(37, 37, 37)
                 .addGroup(jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jText1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel9)
                     .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel8))
@@ -423,6 +423,11 @@ public class GamePreferencesFrame extends javax.swing.JFrame {
         });
 
         jButton2.setText("Save Changes");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -657,20 +662,31 @@ public class GamePreferencesFrame extends javax.swing.JFrame {
 
     private void jTextField8FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField8FocusGained
         this.jTextMessageArea.setText ("Player names should be at least 3 but no more than 12 characters. ");
-        this.jLabelPlayerOne.setText("");
+        this.jLabelPlayerOne.setText ("Current player name:" + this.game.getPlayerA().getName());
+        this.jLabelPlayerTwo.setText ("Current player name:" + this.game.getPlayerB().getName());
     }//GEN-LAST:event_jTextField8FocusGained
 
     private void jTextField7FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField7FocusGained
         this.jTextMessageArea.setText ("Player names should be at least 3 but no more than 12 characters. ");
+        this.jLabelPlayerOne.setText ("Current player name:" + this.game.getPlayerA().getName());
+        this.jLabelPlayerTwo.setText ("Current player name:" + this.game.getPlayerB().getName());
     }//GEN-LAST:event_jTextField7FocusGained
 
-    private void jTextField2FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField2FocusGained
-        this.jTextMessageArea.setText ("Enter a number between 6 and 15. ");          
-    }//GEN-LAST:event_jTextField2FocusGained
+    private void jText1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jText1FocusGained
+        this.jTextMessageArea.setText ("Enter a number between 6 and 15. ");   
+        this.jLabelRows.setText ("Current number of rows:" + this.game.getBoard().getRowCount());
+        this.jLabelColumns.setText ("Current number of columns:" + this.game.getBoard().getColumnCount());
+    }//GEN-LAST:event_jText1FocusGained
 
     private void jTextField3FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jTextField3FocusGained
         this.jTextMessageArea.setText ("Enter a number between 6 and 15. ");   
+        this.jLabelRows.setText ("Current number of rows:" + this.game.getBoard().getRowCount());
+        this.jLabelColumns.setText ("Current number of columns:" + this.game.getBoard().getColumnCount());
     }//GEN-LAST:event_jTextField3FocusGained
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+       
+    }//GEN-LAST:event_jButton2ActionPerformed
 
   
     public static void main(String args[]) {
@@ -741,8 +757,8 @@ public class GamePreferencesFrame extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jText1;
     private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField7;
     private javax.swing.JTextField jTextField8;
