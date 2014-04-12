c0e3cd2bfdf58422e75a11fce079b17791f9e8ad
Terry Wall <twall411@roadrunner.com>
2014-04-12 11:34:44 -0700

    Board will now play after resizing but still has scrollbar and cells too narrow for complete tokens.

--- crazyConnectFour/src/CIT260/PairedProgramming/CrazyConnectFour/Models/Game.java
+++ crazyConnectFour/src/CIT260/PairedProgramming/CrazyConnectFour/Models/Game.java
@@ -76,6 +76,7 @@ public class Game implements Serializable {
         this.board = new Board(Game.NUMBER_OF_ROWS_DEFAULT, Game.NUMBER_OF_COLUMNS_DEFAULT);
         this.board.makeTheBoard();
         
+        
     }
 
     public GameType getGameType() {
